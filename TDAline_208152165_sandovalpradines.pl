9c7108501b000e9b9c4edcad337702d694ff3825
matiassandovalp <matias.sandoval.p@usach.cl>
2024-05-06 11:19:00 -0400

    Implementación parcial TDA section
    +Varios selectores

---
+++ TDAline_208152165_sandovalpradines.pl
@@ -0,0 +1,70 @@
+:- use_module(TDAsection, [section/5, getPoint1/2, getPoint2/2, getDistance/2, getCost/2]).
+
+line(ID, NAME, RAIL_TYPE, SECTIONS, LINE):-
+	integer(ID),
+	string(NAME),
+	string(RAIL_TYPE),
+	is_list(SECTIONS),
+	LINE is [ID, NAME, RAIL_TYPE, SECTIONS].
+
+isLine(LINE):-
+	is_list(LINE),
+	getID(LINE, ID),
+	getName(LINE, NAME),
+	getRailType(LINE, RAIL_TYPE),
+	getSections(LINE, SECTIONS),
+	integer(ID),
+	string(NAME),
+	string(RAIL_TYPE),
+	is_list(SECTIONS).
+
+getID(LINE, ID):-
+	nth0(0, LINE, ID).
+
+getName(LINE, NAME):-
+	nth0(1, LINE, NAME).
+
+getRailType(LINE, RAIL_TYPE):-
+	nth0(2, LINE, RAIL_TYPE).
+
+getSections(LINE, SECTIONS):-
+	nth0(3, LINE, SECTIONS).
+
+
+%LENGTH, DISTANCE Y COST son variables en todo momento.
+lineLength([], 0, 0, 0).
+lineLength(LINE, LENGTH, DISTANCE, COST):-
+	nth0(3, LINE, Sections),
+	len(Sections,LENGTH),
+	dist(Sections, DISTANCE),
+	cost(Sections, COST).
+
+len([], 0).
+len([CAR | CDR], ACUM):-
+	len(CDR, ACUM1),
+	ACUM is ACUM1 + 2.
+
+dist([], 0).
+dist([CAR | CDR], ACUM):-
+    dist(CDR, ACUM1),
+    getDistance(CAR, ActualDistance),
+    ACUM is ACUM1 + ActualDistance.
+
+cost([], 0).
+cost([CAR | CDR], ACUM):-
+	cost(CDR, ACUM1),
+	getCost(CAR, ActualCost),
+	ACUM is ACUM1 + ActualCost.
+
+/*lineSectionLength: TRAYECTO ENTRE 2 ESTACIONES, DISTANCIA Y COSTO 
+lineSectionLength(LINE, StationName1, StationName2, PATH, DISTANCE, COST):-
+*/
+
+lineAddSection(LINE, SECTION, LINEOUT):-
+	getID(LINE, ID),
+	getName(LINE, NAME),
+	getRailType(LINE, RAIL),
+	getSections(LINE, SectionList),
+	append(SectionList, Section, NewSections),%AQUI_REVISAR_COINCIDENCIA_DE_IDS
+	line(ID, NAME, RAIL, NewSections, LINEOUT).
+	
--- TDAsection_208152165_sandovalpradines.pl
+++ TDAsection_208152165_sandovalpradines.pl
@@ -1,3 +1,5 @@
+:- module(TDAsection, [section/5, getPoint1/2, getPoint2/2, getDistance/2, getCost/2]).
+
 :- use_module(TDAstation, [station/5]).
 
 section(POINT1, POINT2, DISTANCE, COST, SECTION):-
@@ -9,4 +11,14 @@ section(POINT1, POINT2, DISTANCE, COST, SECTION):-
 	COST >= 0,
 	SECTION = [POINT1, POINT2, DISTANCE, COST].
 
+getPoint1(SECTION, P1):-
+	nth0(0, SECTION, P1).
 
+getPoint2(SECTION, P2):-
+	nth0(1, SECTION, P2).
+	
+getDistance(SECTION, DIST):-
+    nth0(2, SECTION, DIST).
+
+getCost(SECTION, COST):-
+    nth0(3, SECTION, COST).
